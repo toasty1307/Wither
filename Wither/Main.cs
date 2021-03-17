7bcbdaac0909698be2c47261a5c72e92a14af5aa
Garvit1307 <sharmagarvit614@gmail.com>
2021-03-17 22:15:32 +0530

    Initial Commit Resolved Projects and Solutions Delete Unnecessary files Updated Main Plugin Class

--- Wither/Main.cs
+++ Wither/Main.cs
@@ -1,6 +1,6 @@
 ﻿using BepInEx;
-using BepInEx.Configuration;
 using BepInEx.IL2CPP;
+using BepInEx.Logging;
 using HarmonyLib;
 using Reactor;
 
@@ -9,28 +9,19 @@ namespace Wither
     [BepInPlugin(Id)]
     [BepInProcess("Among Us.exe")]
     [BepInDependency(ReactorPlugin.Id)]
-    public class TemplatePlugin : BasePlugin
+    public class Main : BasePlugin
     {
-        public const string Id = "me.change.please";
+        public const string Id = "me.toasty_marshmallow.Wither";
 
         public Harmony Harmony { get; } = new Harmony(Id);
-
-        public ConfigEntry<string> Name { get; private set; }
-
+        
+        public ManualLogSource Logger { get; private set; }
+        
         public override void Load()
         {
-            Name = Config.Bind("Fake", "Name", ":>");
-
+            Logger = Log;
+            
             Harmony.PatchAll();
-        }
-
-        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
-        public static class ExamplePatch
-        {
-            public static void Postfix(PlayerControl __instance)
-            {
-                __instance.nameText.Text = PluginSingleton<TemplatePlugin>.Instance.Name.Value;
-            }
         }
     }
 }
