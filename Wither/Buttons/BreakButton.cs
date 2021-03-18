46934509f91dd671773bde6541c81de6bdcffb56
toasty marshmallow <sharmagarvit614@gmail.com>
2021-03-18 12:56:23 +0530

    Button Base class and Asset Loader class Added all Buttons

---
+++ Wither/Buttons/BreakButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Wither.Utils;
+
+namespace Wither.Buttons
+{
+    public class BreakButton : Button
+    {
+        public BreakButton(Vector2 _offset, float cooldown) : base(_offset, "BreakImage", cooldown) { }
+
+        protected override void OnClick()
+        {
+            
+        }
+
+        protected override bool CanUse()
+        {
+            return base.CanUse() && PlayerControl.LocalPlayer.Data.IsImpostor;
+        }
+    }
+}
